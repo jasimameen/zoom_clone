d876f7436b4f33e33b8c679939c73eca4528f3b1
Jasim Ameen <89957373+jasimameen@users.noreply.github.com>
2022-09-09 00:07:39 +0530

    added Navigation fuctionality

--- lib/utils/navigation.dart
+++ lib/utils/navigation.dart
@@ -3,4 +3,22 @@ import 'package:flutter/material.dart';
 class Navigation {
   static var navigatorKey = GlobalKey<NavigatorState>();
   static var scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+
+  static Future<T?> pushNamed<T extends Object?>(String routeName,
+          {Object? arguments}) =>
+      navigatorKey.currentState!.pushNamed(routeName, arguments: arguments);
+
+  static Future<T?> pushNamedAndRemoveUntil<T extends Object?>(
+    String routeName,
+    bool Function(Route<dynamic>) predicate, {
+    Object? arguments,
+  }) =>
+      navigatorKey.currentState!
+          .pushNamedAndRemoveUntil(routeName, predicate, arguments: arguments);
+
+  static void pop<T extends Object?>([T? result]) =>
+      navigatorKey.currentState!.pop<T>(result);
+
+  static void popUntil(bool Function(Route<dynamic>) predicate) =>
+      navigatorKey.currentState!.popUntil(predicate);
 }
